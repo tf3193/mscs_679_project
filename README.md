a654871c5cf0850266626b520e782c234af38cc4
taflagg <tf3193@gmail.com>
2020-02-28 08:46:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mscs_679_project
---
+++ src/assign1/Y.scala
@@ -0,0 +1,8 @@
+package assign1
+ /**
+4 * This is the message sent from actor A to actor B.
+5 * It MUST be serializable to be transmitted as a POJO.
+6 * @author R. Coleman
+7 * @param s String message.
+ */
+case class Y(s: String) extends Serializable
